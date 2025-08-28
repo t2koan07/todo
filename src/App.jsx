0f2b47d3be7f139a50281c9934e9bf36ab408ca9
andytrix <andreas.kotala@proton.me>
2025-08-28 11:05:01 +0300

    Part 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
+import Row from './components/Row.jsx'
+
+const url = "http://localhost:3001"
 
 function App() {
-  const [task, setTask] = useState('');
-  const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    axios.get(url)
+      .then(response => {
+        setTasks(response.data)
+      })
+        .catch(error => {
+        alert(error.response.data ? error.response.data.message : error)
+    })
+  }, [])
 
   const addTask = () => {
-    setTasks([...tasks, task]);
-    setTask('');
+    const newTask = { description: task }
+    axios.post(url + "/create", {task: newTask})
+      .then(response => {
+        setTasks([...tasks,response.data])
+        setTask('')
+      })
+        .catch(error => {
+        alert(error.response ? error.response.data.error.message : error)
+    })
   }
 
   const deleteTask = (deleted) => {
-    const withoutRemoved = tasks.filter(item => item!==deleted);
-    setTasks(withoutRemoved);
+    axios.delete(url + "/delete/" + deleted)
+      .then(response => {
+      setTasks(tasks.filter(item => item.id !== deleted))
+    })
+      .catch(error => {
+      alert(error.response ? error.response.data.error.message : error)
+    })
   }
 
   return (
@@ -31,19 +57,11 @@ function App() {
           }}
         />
       </form>
-          <ul>
-            {
-              tasks.map(item => (
-                <li> {item} 
-                  <button 
-                    className='delete-button'
-                    onClick={() => deleteTask(item)}>
-                    Delete
-                  </button>
-                </li>
-              ))
-            }
-          </ul>
+        <ul>
+          {tasks.map(item => (
+            <Row key={item.id} item={item} deleteTask={deleteTask} />
+          ))}
+        </ul>
     </div>
   );
 }
